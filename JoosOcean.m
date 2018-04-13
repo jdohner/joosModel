6796dc5aa6ae1f72018d8826aa79f31f3bf7c312
Julia Dohner <juliadohner@jdohner-imac.ucsd.edu>
2018-04-12 17:03:07 -0700

    First commit

---
+++ JoosOcean.m
@@ -0,0 +1,23 @@
+% joos ocean model
+%
+% author: julia dohner
+%
+% april 12, 2018
+
+% Response function to calculate ocean uptake
+
+for n = 1:length(year)
+     t(n,1) = year(n) - year(1);
+     r(n,1) = t(n,1);
+    %Calculate response function based on HILDA equation in Joos 1996
+     if t(n,1) == 0
+         r(n,2) = 1;
+     elseif t(n,1) <= 2
+         r(n,2)= (1/0.95873)*(0.12935+0.21898*exp(-t(n,1)/0.034569)+0.17003*exp(-t(n,1)/0.26936)...
+             +0.24071*exp(-t(n,1)/0.96083)+0.24093*exp(-t(n,1)/4.9792));
+     else
+         r(n,2) = (1/0.95873)*(0.022936+0.24278*exp(-t(n,1)/1.2679)+0.13963*exp(-t(n,1)/5.2528)...
+                +0.089318*exp(-t(n,1)/18.601)+0.037820*exp(-t(n,1)/68.736)...
+                +0.035549*exp(-t(n,1)/232.3));
+     end
+end
